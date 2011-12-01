557cdc1274fb73a882ab8f04b4ee779969f0bd3e
Alexander  Alemayhu <alexander.alemayhu@googlemail.com>
2011-12-01 16:15:28 +0100

    Initial Commit

---
+++ kanaTable-ipad/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  kanaTable-ipad
+//
+//  Created by Alexander Alemayhu on 01.12.11.
+//  Copyright Flexnor 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
