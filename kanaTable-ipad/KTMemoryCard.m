f38b4dd7906ad8f8c28583c5db1808892ca59157
Alexander Alemayhu <alexander.alemayhu@googlemail.com>
2011-12-25 16:58:35 +0100

    New private method for hiding card content. Call flip card hide or show the label.

--- kanaTable-ipad/KTMemoryCard.m
+++ kanaTable-ipad/KTMemoryCard.m
@@ -7,19 +7,34 @@
 //
 
 #import "KTMemoryCard.h"
+#import "KTKana.h"
+
+@interface KTMemoryCard ()
+-(void) makeOrHide;
+@end
 
 
 @implementation KTMemoryCard
 
-+(id) newCard{
+@synthesize isFacedUp;
 
-    return [[[self alloc] init] autorelease];
++(id) withKey:(NSUInteger)key{
+    
+    return [[[self alloc] initCardWithKey:key] autorelease];
 }
 
--(id) init{
+-(id) initCardWithKey:(NSUInteger)key{
     
     if (self = [super initWithFile:@"card_tmp.png"]) {
         
+        isFacedUp = NO;
+        kana = [[KTKana alloc] initWithKanaID:key kanaType:HIRAGANA];
+        
+        label = [CCLabelTTF labelWithString:kana.kana fontName:@"Arial" fontSize:50];
+        [label setColor:ccBLACK];
+        label.position = ccp(self.contentSize.width/2, self.contentSize.height/2);
+        [label setVisible:isFacedUp];
+        [self addChild:label];
     }
     
     return self;
@@ -27,7 +42,19 @@
 
 -(void) flipCard{
     
-    
+    //Code from cocos2d forum
+    float d = 0.25f;
+    id firstAction = [CCOrbitCamera actionWithDuration:d/2 radius:1 deltaRadius:0 angleZ:0 deltaAngleZ:90 angleX:0 deltaAngleX:0];
+    id secondAction = [CCOrbitCamera actionWithDuration:d/2 radius:1 deltaRadius:0 angleZ:270 deltaAngleZ:90 angleX:0 deltaAngleX:0];
+    id thirdAction = [CCCallFuncN actionWithTarget:self selector:@selector(makeOrHide)];
+    [self runAction: [CCSequence actions:firstAction, thirdAction, secondAction, nil]];        
 }
+
+-(void) makeOrHide{
+    
+    isFacedUp = !isFacedUp;
+    [label setVisible:isFacedUp];
+}
+
 
 @end
