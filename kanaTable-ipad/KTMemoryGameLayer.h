9ac99b703321c2aa38e6bdb8e7d39ea991046759
Alexander Alemayhu <alexander.alemayhu@googlemail.com>
2011-12-25 17:09:56 +0100

    Remove menu. Use KTLevel for level init.

--- kanaTable-ipad/KTMemoryGameLayer.h
+++ kanaTable-ipad/KTMemoryGameLayer.h
@@ -8,15 +8,17 @@
 
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
+#import "KTGlobal.h"
 
 @interface KTMemoryGameLayer : CCLayer {
-    CCSprite *background;    
-    
-    //Menu for aligning items.
-    CCMenu *menu;    
+    CCSprite *background;        
+    KTLevel level;    
+    NSMutableArray *cards;
 }
 
 +(CCScene *) scene;
++(id) withLevel:(KTLevel)lvl;
 
 -(void) setupGameBoard;
+-(id) initWithLevel:(KTLevel)lvl;
 @end
